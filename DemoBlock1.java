00babc2220ad2abb2dec861f938f0fde478fd14f
DevmanushRaky <rakesucluci@gmail.com>
2022-07-18 22:23:06 +0530

    Blocks in java 1

---
+++ DemoBlock1.java
@@ -0,0 +1,22 @@
+
+class BTest1{  //SubClass
+    static int x = 20;
+    static{
+        System.out.println("----SubClass----");
+        System.out.println("The value of x :"+x);
+    }
+}
+
+public class DemoBlock1 {  //Main Class
+   static int z=10;
+    public static void main(String[] args) {
+       int x= 500;
+        System.out.println("------This is main ------");
+        System.out.println("The value of x:"+BTest1.x );
+    }
+    static{
+        System.out.println("----MainClass Static block-----");
+        System.out.println("The value of z :"+ z);
+    }
+    
+}
