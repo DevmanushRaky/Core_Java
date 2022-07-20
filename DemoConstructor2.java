60ab92ce88af9cc5ca7fb4e152a57ced6165c1f2
DevmanushRaky <rakesucluci@gmail.com>
2022-07-20 12:21:26 +0530

    Constructor with parameter & priority

--- DemoConstructor2.java
+++ DemoConstructor2.java
@@ -1,22 +1,33 @@
+import java.util.Scanner;
 class CTest2{
     int x= 10;
     static int y= 20;    
     static{
         System.out.println("---This is Static block in SUbclass---");
-        System.out.println("The value of y: "+y);
+        System.out.println("The value of : "+y);
     }
     {
         System.out.println("---This is instance Block---");
         System.out.println("The value of x: "+x);
     }
-    CTest2(){
+    CTest2(int x){
         System.out.println("--This is Constructor---");
         System.out.println("The value of x:"+ x);
+    }
+    void dis(int x){
+        System.out.println("----This is non-static method (Instance method) ---- ");
+        System.out.println("The value of x: "+x);
     }
 }
 
 public class DemoConstructor2 {
     public static void main(String[] args) {
-        CTest2 ob = new CTest2();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter value for constructer : ");
+        int v1 = s.nextInt();
+        System.out.print("Enter value for method : ");
+        int v2 = s.nextInt();
+        CTest2 ob = new CTest2(v1); //constructor call with parameter
+        ob.dis(v2);
     }
 }
